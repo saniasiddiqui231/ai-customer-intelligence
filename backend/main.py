4c195cc800f444dfe74c1a0f0dec8f1411938d1f
Sania Siddiqui <sania.siddiqui231@gmail.com>
2026-06-09 16:54:33 +0530

    modified main.puy

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173"
+        "https://ai-customer-intelligence.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
